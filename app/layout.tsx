9b226263821211d895bb3322dc6eaf6d63ae8b9b
Thompson Solomon <thompsonsolomon123@gmail.com>
2026-02-17 00:28:16 +0100

    change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,15 +11,19 @@ export const metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        // url: '/icon-light-32x32.png',
+                url: '/asset/logofull.png',
+
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        // url: '/icon-dark-32x32.png',
+                url: '/asset/logofull.png',
+
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/asset/logoround.png',
         type: 'image/svg+xml',
       },
     ],
